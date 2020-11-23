39d683d65c478bdcb917ffeb51189865024b658a
Stanislav Burovsky <sburovsky@gmail.com>
2020-11-23 21:34:42 +0300

    Added Class Point

---
+++ tetris/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tetris
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char c;
+
+        public void draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
+        }
+    }
+}
--- tetris/Program.cs
+++ tetris/Program.cs
@@ -9,21 +9,23 @@ namespace tetris
             Console.SetWindowSize(40, 30);
             Console.SetBufferSize(40, 30);
 
-            int x1 = 2;
-            int y1 = 3;
-            char c1 = '*';
+            Point p1 = new Point();
+            p1.x = 2;
+            p1.y = 3;
+            p1.c = '*';
 
-            Draw(x1, y1, c1);
+            p1.draw();
+
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.c = '*';
+
+            p2.draw();
 
             Console.ReadLine();
         }
 
-        static void Draw(int x, int y, char c)
-        { 
-            Console.SetCursorPosition(x, y);
-            Console.Write(c);
-        }
-            
 
     }
 }
